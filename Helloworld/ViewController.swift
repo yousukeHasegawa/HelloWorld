b7ebc4fd59018ac2217fdbb6686b5a10448c7594
yousuke.hasegawa <hasegawa.yousuke.mn@mitsubishichem-sys.co.jp>
2021-07-16 12:00:06 +0900

    Initial Commit

---
+++ Helloworld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Helloworld
+//
+//  Created by Yousuke Hasegawa on 2021/07/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
